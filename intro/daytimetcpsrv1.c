c63a383478c338bd9a5b6976fbea46fa25db91b6
cwd-wenzhou <619715109@qq.com>
2021-03-30 19:21:20 +0800

    2021331

--- intro/daytimetcpsrv1.c
+++ intro/daytimetcpsrv1.c
@@ -9,27 +9,28 @@ main(int argc, char **argv)
 	struct sockaddr_in	servaddr, cliaddr;
 	char				buff[MAXLINE];
 	time_t				ticks;
-
-	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+	listenfd = Socket(AF_INET, SOCK_STREAM, 0);//tcp连接
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family      = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port        = htons(13);	/* daytime server */
-
 	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
 	Listen(listenfd, LISTENQ);
 
 	for ( ; ; ) {
 		len = sizeof(cliaddr);
-		connfd = Accept(listenfd, (SA *) &cliaddr, &len);
+		connfd = Accept(listenfd, (SA *) &cliaddr, &len);//返回的是已完成的连接
 		printf("connection from %s, port %d\n",
 			   Inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)),
 			   ntohs(cliaddr.sin_port));
 
         ticks = time(NULL);
         snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+
+		//char *p1 = "This is a c test code";
+        //Write(connfd, p1, strlen(buff));
         Write(connfd, buff, strlen(buff));
 
 		Close(connfd);
