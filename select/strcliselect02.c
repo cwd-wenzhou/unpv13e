994f06709a6584c5bff634d3bcb9769b57c18112
cwd-wenzhou <619715109@qq.com>
2021-04-04 15:41:05 +0800

    增加selelct部分注释

--- select/strcliselect02.c
+++ select/strcliselect02.c
@@ -11,8 +11,10 @@ str_cli(FILE *fp, int sockfd)
 	stdineof = 0;
 	FD_ZERO(&rset);
 	for ( ; ; ) {
+		//若stdineof==0，标准输入可读
 		if (stdineof == 0)
 			FD_SET(fileno(fp), &rset);
+
 		FD_SET(sockfd, &rset);
 		maxfdp1 = max(fileno(fp), sockfd) + 1;
 		Select(maxfdp1, &rset, NULL, NULL, NULL);
@@ -20,7 +22,7 @@ str_cli(FILE *fp, int sockfd)
 		if (FD_ISSET(sockfd, &rset)) {	/* socket is readable */
 			if ( (n = Read(sockfd, buf, MAXLINE)) == 0) {
 				if (stdineof == 1)
-					return;		/* normal termination */
+					return;		/*已经输入过EOF，是 normal termination */
 				else
 					err_quit("str_cli: server terminated prematurely");
 			}
@@ -32,7 +34,7 @@ str_cli(FILE *fp, int sockfd)
 		//调用read时已到达文件末尾，返回值为0，进入if里面
 			if ( (n = Read(fileno(fp), buf, MAXLINE)) == 0) {
 				stdineof = 1;
-				Shutdown(sockfd, SHUT_WR);	/* send FIN */
+				Shutdown(sockfd, SHUT_WR);	/* send FIN   SHUT_WR：关闭连接写的这一半*/
 				FD_CLR(fileno(fp), &rset);
 				continue;
 				//从此以后只接受数据，不发送数据
