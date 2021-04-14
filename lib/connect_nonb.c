15255def944016f539df148b9bd5cbcaf94a8639
cwd-wenzhou <619715109@qq.com>
2021-04-14 21:02:14 +0800

    添加注释

--- lib/connect_nonb.c
+++ lib/connect_nonb.c
@@ -8,11 +8,13 @@ connect_nonb(int sockfd, const SA *saptr, socklen_t salen, int nsec)
 	fd_set			rset, wset;
 	struct timeval	tval;
 
+	//用Fcntl设置非阻塞
 	flags = Fcntl(sockfd, F_GETFL, 0);
 	Fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
 
 	error = 0;
 	if ( (n = connect(sockfd, saptr, salen)) < 0)
+	//期望的错误是EINPROGRESS，表示连接建立已经启动但是尚未完成，其他错误则需要处理
 		if (errno != EINPROGRESS)
 			return(-1);
 
@@ -36,6 +38,8 @@ connect_nonb(int sockfd, const SA *saptr, socklen_t salen, int nsec)
 
 	if (FD_ISSET(sockfd, &rset) || FD_ISSET(sockfd, &wset)) {
 		len = sizeof(error);
+		//getsockopt取得套接字的待处理错误，若成功建立连接则为0；<0需要错误处理
+		//SOL_SOCKET，SO_ERROR常值的设定使getsockopt功能为获取待处理错误并清除
 		if (getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &error, &len) < 0)
 			return(-1);			/* Solaris pending error */
 	} else
@@ -43,6 +47,9 @@ connect_nonb(int sockfd, const SA *saptr, socklen_t salen, int nsec)
 
 done:
 	Fcntl(sockfd, F_SETFL, flags);	/* restore file status flags */
+
+
+	//恢复套接字的文字状态标志并返回
 
 	if (error) {
 		close(sockfd);		/* just in case */
